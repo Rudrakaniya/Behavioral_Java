a5d68ea74bdc6f7199a693f207fe89f84c4c25fa
RUDRA KANIYA <41951699+Rudrakaniya@users.noreply.github.com>
2020-04-08 15:48:48 +0530

    largest burst time scheduler
    Design a scheduler following a non-preemptive scheduling approach to schedule the
    processes that arrive at different units and having burst time double the arrival time. The scheduler
    selects the process with the largest burst time from the queue for the execution. The process is not being
    preempted until it finishes its service time. Compute the average waiting time and average
    turnaround time.

---
+++ os/schedulingAlgorithm.cpp
@@ -0,0 +1,72 @@
+//break_the_bond
+//jatt_james_bond
+#include<bits/stdc++.h>
+using namespace std;
+
+int largest(int n, vector<int> burstTime){
+	int l = 0, index = 0;
+	for (int i = 0; i < n; ++i)
+	{
+		if(burstTime[i] > l){
+			l = burstTime[i];
+			index = i;
+		}
+	}
+	return index;
+}
+
+int main()
+{
+	int processes;
+	cout << "Enter the number of processes __";
+	cin >> processes;
+	vector<int> arrivalTime(processes);
+	vector<int> burstTime(processes);
+	
+
+	cout << endl
+		 << "Enter the Arrival time of each process: -" << endl;
+
+	for (int i = 0; i < processes;++i){
+		cout << "Enter the arival time of P" << i << " process __";
+		cin >> arrivalTime[i];
+		burstTime[i] = arrivalTime[i] << 1;
+	}
+	vector<int> completionTime(processes);
+	vector<int> turnAroundTime(processes);
+	vector<int> waitingTime(processes);
+	vector<int> temp = burstTime;
+	int exeTime{0};
+
+
+	cout << endl;
+
+	for (int i = 0; i < processes; ++i)
+	{
+		int quant = largest(processes, temp);
+		completionTime[quant] = exeTime + burstTime[quant];
+		exeTime = completionTime[quant];
+		temp[quant] = -1;
+		turnAroundTime[quant] = completionTime[quant] - arrivalTime[quant];
+		int sg = turnAroundTime[quant] - burstTime[quant];
+		(sg < 0) ? sg = 0 : sg;
+		waitingTime[quant] = sg; 
+	}
+
+
+	int averageWaitingTime{0};
+	int averageTurnaroundTime{0};
+	for (int i = 0; i < processes; ++i)
+	{
+		cout << "Completion time of P" << i << " process is = " << completionTime[i] << ",  Turn Around Time = "<< turnAroundTime[i]<<"  And Waiting Time = "<< waitingTime[i] << endl;
+		averageWaitingTime += waitingTime[i];
+		averageTurnaroundTime += turnAroundTime[i];
+	}
+
+	cout << endl
+		 << "Average waiting time = " << ((float)averageWaitingTime / processes) << endl;
+
+	cout << endl
+		 << "Average turnaround time = " << ((float)averageTurnaroundTime / processes) << endl;
+	return 0;
+}
